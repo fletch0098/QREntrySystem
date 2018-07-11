ddf246bc78cdd86d757219882bc38da340c18458
fletch0098 <brian.m.fletcher@gmail.com>
2018-07-10 22:12:59 -0400

    initialize

---
+++ QREntry.WebAPI/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using NLog.Web;
+
+namespace QREntry.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var logger = services.GetRequiredService<ILogger<Program>>();
+
+                logger.LogInformation("Application Start");
+
+                try
+                {
+                    //var context = services.GetRequiredService<ComputerContext>();
+
+                    //var dbInitializer = services.GetRequiredService<DbInitializer>();
+
+                    //dbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+
+                host.Run();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    logging.AddAzureWebAppDiagnostics();
+                    logging.AddDebug();
+                })
+
+                .UseNLog()  // NLog: setup NLog for Dependency injection
+
+                .Build();
+    }
+}
---
+++ QREntry.WebAPI/Startup.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using NLog.Web;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace QREntry.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+
+            //EF DB
+            //services.AddDbContext<ComputerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+            //b => b.MigrationsAssembly("ComputerWebAPI")));
+
+            //DI
+            //services.AddSingleton(typeof(IDataRepository<Computer, long>), typeof(ComputerManager));
+            //services.AddTransient(typeof(IDataRepository<Computer, long>), typeof(ComputerManager));
+            //services.AddSingleton(typeof(IDataRepository<Memory, long>), typeof(MemoryManager));
+            //services.AddTransient(typeof(IDataRepository<Memory, long>), typeof(MemoryManager));
+            //services.AddTransient<ComputerManager>();
+            //services.AddTransient<DbInitializer>();
+            //services.AddTransient<ComputerLibrary.Business_Logic.ComputerBAL>();
+
+            //CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("LocalDev",
+                    policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+
+            //JSON LOOPS
+            services.AddMvc().AddJsonOptions(options => {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            //MVC With Options
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("LocalDev"));
+            });
+
+            //Swagger API
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "QR Entry System API", Version = "v1.0" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //DEV Setting
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //SWAGGER
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value);
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "QR Entry System API V1.0");
+            });
+
+            app.UseMvc(routes =>
+            {
+                //TODO: Needed?
+                routes.MapRoute(
+                name: "default",
+                template: "{controller}/{action}/{id?}",
+                defaults: new { controller = "swagger", action = "Index" });
+            });
+
+            //Configure Log
+            env.ConfigureNLog("nlog.config");
+        }
+    }
+}
